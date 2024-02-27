eb98cf5d1b62179da995c7ad9a9913ef0e864640
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-02-26 17:00:17 -0800

    Countdown function works

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import time
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -15,11 +15,20 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    pass
+    count_down(WORK_MIN * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    if count_min < 10:
+        count_min = f"0{count_min}"
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=(f"{count_min}:{count_sec}"))
     if count > 0:
         window.after(1000, count_down, count - 1)
 
@@ -39,7 +48,6 @@ tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
 canvas.grid(row=1, column=1)
-count_down(5)
 
 # Start button
 start = Button(text="Start", command=start_timer, bg=GREEN, highlightthickness=0, font=(FONT_NAME), cursor="hand2")
