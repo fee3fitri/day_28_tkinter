12ffc7532d83d1e4c7f63fd3e96f98b0ea0d7fa8
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-02-26 16:45:26 -0800

    Add countdown function

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import time
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -12,9 +13,15 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    pass
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Create Tkinter window
@@ -30,14 +37,12 @@ timer.grid(row=0, column=1)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
+timer_text = canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
 canvas.grid(row=1, column=1)
+count_down(5)
 
 # Start button
-def start():
-    pass
-
-start = Button(text="Start", command=start, bg=GREEN, highlightthickness=0, font=(FONT_NAME), cursor="hand2")
+start = Button(text="Start", command=start_timer, bg=GREEN, highlightthickness=0, font=(FONT_NAME), cursor="hand2")
 start.grid(row=2, column=0)
 start.config(padx=10)
 
